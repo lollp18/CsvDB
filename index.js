811fb78e6ba6f1b15195d3e358f983b8a0172124
lollp18 <lorenzo123696@gmail.com>
2022-12-25 17:46:07 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+"use strict";
+const port = 80;
+const http = require("http");
+
+const express = require("express");
+
+const bodyParser = require("body-parser");
+const app = express();
+
+const server = http.createServer(app);
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(express.json());
+
+let tabels = [];
+
+app.post("/", async (req, res) => {
+  console.log(req.body);
+
+  tabels = await req.body;
+
+  res.status(201).send(JSON.stringify("erstelt"));
+});
+
+app.get("/", async (req, res) => {
+  res.send(JSON.stringify(tabels));
+});
+app.delete("/", async (req, res) => {
+  const tabelID = await req.body.id;
+
+  tabels.splice(tabelID, 1);
+
+  console.log(tabels);
+  res.status(201).send(JSON.stringify("Gelöscht"));
+});
+server.listen(port, () => {
+  console.log("geht");
+});
