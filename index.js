ed4558e3b9ad625de770d742c617dc75790a1267
lollp18 <lorenzo123696@gmail.com>
2023-02-15 17:41:21 +0100

    temp

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-"use strict";
-const port = 80;
-const http = require("http");
+"use strict"
+const port = 80
+const http = require("http")
 
-const express = require("express");
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const app = express()
 
-const bodyParser = require("body-parser");
-const app = express();
+const server = http.createServer(app)
 
-const server = http.createServer(app);
+app.use(bodyParser.json({ limit: "50mb" }))
+app.use(express.json())
 
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(express.json());
-
-let tabels = [];
+let tabels = []
 
 app.post("/", async (req, res) => {
-  console.log(req.body);
+  console.log(req.body)
 
-  tabels = await req.body;
+  tabels = await req.body
 
-  res.status(201).send(JSON.stringify("erstelt"));
-});
+  res.status(201).send(JSON.stringify("erstelt"))
+})
 
 app.get("/", async (req, res) => {
-  res.send(JSON.stringify(tabels));
-});
+  res.send(JSON.stringify(tabels))
+})
 app.delete("/", async (req, res) => {
-  const tabelID = await req.body.id;
+  const tabelID = await req.body.id
 
-  tabels.splice(tabelID, 1);
+  tabels.splice(tabelID, 1)
 
-  console.log(tabels);
-  res.status(201).send(JSON.stringify("Gelöscht"));
-});
+  console.log(tabels)
+  res.status(201).send(JSON.stringify("Gelöscht"))
+})
 server.listen(port, () => {
-  console.log("geht");
-});
+  console.log("geht")
+})
