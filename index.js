4b5d27ff70d2cc4fadd93bc64c15014e86670c8f
lollp18 <lorenzo123696@gmail.com>
2023-03-10 18:15:14 +0100

    Login

--- index.js
+++ index.js
@@ -5,34 +5,16 @@ const http = require("http")
 const express = require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser")
+const UserRouter = require("./routers/Users")
+
 const app = express()
 
 const server = http.createServer(app)
 
 app.use(bodyParser.json({ limit: "50mb" }))
 app.use(express.json())
+app.use("/users", UserRouter)
 
-let tabels = []
-
-app.post("/", async (req, res) => {
-  console.log(req.body)
-
-  tabels = await req.body
-
-  res.status(201).send(JSON.stringify("erstelt"))
-})
-
-app.get("/", async (req, res) => {
-  res.send(JSON.stringify(tabels))
-})
-app.delete("/", async (req, res) => {
-  const tabelID = await req.body.id
-
-  tabels.splice(tabelID, 1)
-
-  console.log(tabels)
-  res.status(201).send(JSON.stringify("Gelöscht"))
-})
 server.listen(port, () => {
   console.log("geht")
 })
---
+++ routers/Users.js
@@ -0,0 +1,141 @@
+const express = require("express")
+const router = express.Router()
+const emailValidator = require("deep-email-validator")
+const Users = new Map([
+  [
+    "Hans",
+    {
+      Username: "Hans",
+      Email: "Hans123@gmail.com",
+      Password: 12,
+
+      Tabellen: [
+        {
+          TabelName: "no Tabel",
+          data: [
+            [
+              { activ: false, zellenInhalt: "" },
+              { activ: false, zellenInhalt: "" },
+              { activ: false, zellenInhalt: "" },
+              { activ: false, zellenInhalt: "" },
+              { activ: false, zellenInhalt: "" },
+              { activ: false, zellenInhalt: "" },
+              { activ: false, zellenInhalt: "" },
+              { activ: false, zellenInhalt: "" },
+            ],
+            [
+              { activ: false, zellenInhalt: "" },
+              { activ: false, zellenInhalt: "" },
+              { activ: false, zellenInhalt: "" },
+              { activ: false, zellenInhalt: "" },
+              { activ: false, zellenInhalt: "" },
+              { activ: false, zellenInhalt: "" },
+              { activ: false, zellenInhalt: "" },
+              { activ: false, zellenInhalt: "" },
+            ],
+
+            [
+              { activ: false, zellenInhalt: "" },
+              { activ: false, zellenInhalt: "" },
+              { activ: false, zellenInhalt: "" },
+              { activ: false, zellenInhalt: "" },
+              { activ: false, zellenInhalt: "" },
+              { activ: false, zellenInhalt: "" },
+              { activ: false, zellenInhalt: "" },
+              { activ: false, zellenInhalt: "" },
+            ],
+          ],
+          lastZelle: {
+            zeile: undefined,
+            spalten: undefined,
+            zellenInhalt: "",
+            activ: false,
+          },
+          currentZelle: {
+            zeile: undefined,
+            spalten: undefined,
+            zellenInhalt: "",
+            activ: false,
+          },
+        },
+      ],
+    },
+  ],
+])
+console.log(Users)
+const User = class {
+  Tabellen = []
+  constructor(Username, Email, Password) {
+    this.Username = Username
+    this.Email = Email
+    this.Password = Password
+  }
+}
+
+// POST
+
+router.post("/Registrieren", async (req, res) => {
+  try {
+    const UserData = await req.body
+
+    if (Users.has(UserData.Username).valueOf(false)) {
+      res.status(201).send(JSON.stringify("UserTaken"))
+    } else if (
+      (await emailValidator.validate(UserData.Email)).valid === false
+    ) {
+      res.status(201).send(JSON.stringify("EmailInvalid"))
+    } else if (UserData.Password !== UserData.PasswordWiederholen) {
+      res.status(201).send(JSON.stringify("PasswordWiederholen"))
+    } else {
+      Users.set(
+        `${UserData.Username}`,
+        new User(UserData.Username, UserData.Email, UserData.Password)
+      )
+
+      res.status(201).send(JSON.stringify("UserErstelt"))
+    }
+    console.log(Users)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+router.post("/Anmelden", async (req, res) => {
+  try {
+    const UserData = await req.body
+
+    if (Users.has(UserData.Username) === false) {
+      res.status(201).send(JSON.stringify("NoUser"))
+    } else if (Users.get(UserData.Username).Password != UserData.Password) {
+      res.status(201).send(JSON.stringify("Password"))
+    } else {
+      res
+        .status(201)
+        .send(JSON.stringify({ Angemeldet: true, Username: UserData.Username }))
+    }
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+// GET
+
+router.get("/:user/Tabellen", async (req, res) => {
+  res.status(201).send(JSON.stringify(Users.get(req.params.user).Tabellen))
+})
+
+// PUT
+
+router.put("/:user/Tabellen", async (req, res) => {
+  Users.get(req.params.user).Tabellen = await req.body
+  res.status(201).send(JSON.stringify("TabellenUpdated"))
+})
+
+router.delete("/:user/Tabellen", async (req, res) => {
+  const tabelID = await req.body.id
+
+  Users.get(req.params.user).Tabellen.splice(tabelID, 1)
+
+  res.status(201).send(JSON.stringify("Gelöscht"))
+})
+module.exports = router
